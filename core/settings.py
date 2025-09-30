a687a8e7fd7de9dbe2e635341838aadacca1bec7
xplorian12 <150617075+xplorian12@users.noreply.github.com>
2025-09-30 08:36:02 -0700

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",") if not os.environ.get("DJANGO_DEBUG") else ["*"]
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
